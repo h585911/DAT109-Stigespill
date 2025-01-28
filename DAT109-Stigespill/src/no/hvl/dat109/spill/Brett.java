0ebecb4759061e2e14a42a3cfe47e47bb6c4c8fa
h585911 <585911@stud.hvl.no>
2025-01-28 18:09:42 +0100

    Lagt til persistence

--- DAT109-Stigespill/src/no/hvl/dat109/spill/Brett.java
+++ DAT109-Stigespill/src/no/hvl/dat109/spill/Brett.java
@@ -11,6 +11,15 @@ public class Brett extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private final Rute[][] ruter;
 	
+	/**
+	 * Brett-metoden skal opprette et brett hvor vi har ett grid-system som består av 10x10 = 100 Ruter.
+	 * 
+	 * Den første Ruten (rute 1) skal plasseres helt nede til venstre.
+	 * Den siste Ruten (rute 100) skal plasseres helt oppe til venstre.
+	 * 
+	 * Dette betyr at vi må veksle mellom rekkefølgen på radene.
+	 * 
+	 */
 	public Brett() {
 		
 		setLayout(new GridLayout(10, 10));
