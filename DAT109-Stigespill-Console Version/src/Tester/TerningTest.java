467a993906045a66f789eef46052ba931ebad329
Laiba Khan <669827@stud.hvl.no>
2025-01-30 17:04:58 +0100

    Mockito
    Mockito dependencies

--- DAT109-Stigespill-Console Version/src/Tester/TerningTest.java
+++ DAT109-Stigespill-Console Version/src/Tester/TerningTest.java
@@ -1,5 +1,29 @@
 package Tester;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import no.hvl.dat109.spill.Terning;
 public class TerningTest {
 
+	private Terning terning;
+	
+	@BeforeEach
+	void setUp() {
+		terning= new Terning();
+	}
+	@Test
+	void testTerningVerdi() {
+		int verdi = terning.trill();
+		assertTrue(verdi>=1 && verdi <=6); //Terningkast skal være mellom 1-6
+	}
+	
+	@RepeatedTest(100)
+	void testTilfeldigVerdi() {
+		int verdi = terning.trill();
+		assertTrue(verdi>=1 && verdi <=6); //Terningkast skal være mellom 1-6
+	}
+	
 }
